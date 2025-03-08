39d16cb9516bd97f6e1db1c8983346d9f56ba270
NITESHMEHTA97 <47318026+NITESHMEHTA97@users.noreply.github.com>
2025-03-08 13:28:23 +0000

    type builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Set<String> commands = Set.of("echo", "exit", "type");
+        String input,typeSubstring;
         while(true){
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
+            input = scanner.nextLine();
             if(input.startsWith("echo")){
-                 System.out.println(input.replace("echo ", ""));
+                 System.out.println(input.substring(5));
+            }else if(input.startsWith("type")){
+                typeSubstring = input.substring(5);
+                if (commands.contains(typeSubstring)) {
+                    System.out.printf("%s is a shell builtin%n", typeSubstring);
+                  } else {
+                    System.out.printf("%s: not found%n", typeSubstring);
+                  }
             }else if (input.equals("exit 0")){
-                break;
+                break;            
             }else{
                 System.out.println(input + ": command not found");
             }
