0ba5f13908710cebd58c525fc1bcb42e365bd629
NITESHMEHTA97 <47318026+NITESHMEHTA97@users.noreply.github.com>
2025-03-08 13:05:23 +0000

    exit button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        while(true){
-            System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
+        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        do{
             System.out.println(input + ": command not found");
-        }
+            System.out.print("$ ");
+            input = scanner.nextLine();
+            
+        }while(!input.matches("exit 0"));
+        scanner.close();
     }
 }
